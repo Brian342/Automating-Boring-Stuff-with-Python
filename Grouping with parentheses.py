4ec9f7cb037636e8e3a4ab6ac68ff0bf5e7158c9
Brian kimanzi <migelbrian3@gmail.com>
2025-06-04 02:40:50 +0300

    New Automation Codes

--- Grouping with parentheses.py
+++ Grouping with parentheses.py
@@ -3,3 +3,6 @@ import re
 phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 mo = phoneNumberRegex.search('My number is 415-555-4242.')
 mo.group(2)
+
+if __name__ == "__main__":
+    pass
