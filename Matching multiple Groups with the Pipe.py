d80c869657f5f3e46e8964061013049f2ead7eea
Brian kimanzi <migelbrian3@gmail.com>
2025-06-11 02:21:49 +0300

    New Automation Codes

--- Matching multiple Groups with the Pipe.py
+++ Matching multiple Groups with the Pipe.py
@@ -1,11 +1,12 @@
 import re
 # this character | is called Pipe used in matching one of many expressions
 
-heroRegex = re.compile(r'Batman | Tina Fey')
+heroRegex = re.compile(r'Batman | Tina Fey', re.IGNORECASE)
 # mo1 = heroRegex.search('Batman and Tina Fey')
-
 # print(mo1.group())
 
-mo2 = heroRegex.search('Tina Fey and Batman')
+
+heroRegex2 = re.compile('Tina Fey | Batman', re.IGNORECASE)
+mo2 = heroRegex2.search('Tina Fey and Batman')
 
 print(mo2.group())
