5df98b6583acc7dfa1cf5aa10fbafdf9ce31cc42
Brian kimanzi <migelbrian3@gmail.com>
2025-06-11 02:29:16 +0300

    New Automation Codes

--- Matching multiple Groups with the Pipe.py
+++ Matching multiple Groups with the Pipe.py
@@ -14,4 +14,4 @@ mo2 = heroRegex2.search('Tina Fey and Batman')
 batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
 mo = batRegex.search('Batmobile lost a wheel')
 print(mo.group()) # returns the full matching text batmobile
-print(mo.group(1)) # returns just the
+print(mo.group(1)) # returns just the part of the matched text inside the first parenthesis group
