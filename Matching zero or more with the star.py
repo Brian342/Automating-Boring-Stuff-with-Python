d28e0b20d26a5d91ebb5002a512c37afdafd479f
Brian kimanzi <migelbrian3@gmail.com>
2025-06-11 02:48:25 +0300

    New Automation Codes

--- Matching zero or more with the star.py
+++ Matching zero or more with the star.py
@@ -5,3 +5,9 @@ import re
 batRegex = re.compile(r'Bat(wo)*man')
 mo1 = batRegex.search('The adventures of Batman')
 print(mo1.group())
+
+mo2 = batRegex.search('The adventures of Batwoman')
+print(mo2.group())
+
+mo3 = batRegex.search('The adventures of Batwowowowoman')
+print(mo3.group())
