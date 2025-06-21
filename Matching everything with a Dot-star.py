313c1c50bbb0bf68f073d3c01d123069be7f992e
Brian kimanzi <migelbrian3@gmail.com>
2025-06-21 15:53:54 +0300

    New Automation Codes

--- Matching everything with a Dot-star.py
+++ Matching everything with a Dot-star.py
@@ -1,6 +1,7 @@
 import re
 
 nameRegex = re.compile(r'First Name: (.*) Last Name:(.*)')
-mo = nameRegex.search('First Name: Al Last Name: Sweigart')
-print(mo.group(1))
-print(mo.group(2))
+# mo = nameRegex.search('First Name: Al Last Name: Sweigart')
+# print(mo.group(1))
+# print(mo.group(2))
+
