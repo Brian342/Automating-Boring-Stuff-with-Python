0b3f5e61fcc0fe376da1f339757bcbb3d4228985
Brian kimanzi <migelbrian3@gmail.com>
2025-06-21 15:27:48 +0300

    New Automation Codes

--- The caret and Dollar sign characters.py
+++ The caret and Dollar sign characters.py
@@ -1,6 +1,7 @@
 import re
 
 beginsWithHello = re.compile(r'^Hello')
-print(beginsWithHello.search('Hello World'))
-print(beginsWithHello.search('He said Hello.') is None)
+# print(beginsWithHello.search('Hello World'))
+# print(beginsWithHello.search('He said Hello.') is None)
 
+# The r'\d$
