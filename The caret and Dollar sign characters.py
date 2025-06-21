bda0b47b5de78aa62147a7e8429bacbc2cf2e676
Brian kimanzi <migelbrian3@gmail.com>
2025-06-21 15:40:09 +0300

    New Automation Codes

--- The caret and Dollar sign characters.py
+++ The caret and Dollar sign characters.py
@@ -6,7 +6,9 @@ beginsWithHello = re.compile(r'^Hello')
 
 # The r'\d$ expression string matches strings that end with a numerical character from 0 to 9
 endsWithNumber = re.compile(r'\d$')
-print(endsWithNumber.search('Your number is 42'))
-print(endsWithNumber.search('Your number is forty two') is None)
+# print(endsWithNumber.search('Your number is 42'))
+# print(endsWithNumber.search('Your number is forty two') is None)
 
 # The r'\d+$ expression string matches strings that both begin and end with one or more numerical characters
+wholeStringIsNum = re.compile(r'^\d+$')
+print(wholeStringIsNum.search('1234567890'))
