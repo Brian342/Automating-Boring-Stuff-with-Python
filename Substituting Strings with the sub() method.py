04663529703efa420e6bb52cd8d1936001189da9
Brian kimanzi <migelbrian3@gmail.com>
2025-06-27 05:51:12 +0300

    New Automation Codes

--- Substituting Strings with the sub() method.py
+++ Substituting Strings with the sub() method.py
@@ -5,4 +5,5 @@ namesRegex = re.compile(r'Agent \w+')
 
 # Names censor
 agentNameRegex = re.compile(r'Agent (\w)\w*')
-print(agentNameRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve knew'))
+print(agentNameRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve knew '
+                                    'Agent'))
