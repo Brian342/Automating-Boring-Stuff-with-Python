6f81cd099cbe4228ad8bcf43f72f9a50c2479650
Brian kimanzi <migelbrian3@gmail.com>
2025-07-02 01:18:26 +0300

    New Automation Codes

--- Project_Phone Number and Email Extractor.py
+++ Project_Phone Number and Email Extractor.py
@@ -33,4 +33,4 @@ for groups in emailRegex.findall(text):
 # step4: joins the matches into a string for the clipboard
 # copy result to the clipboard
 if len(matches) > 0:
-    pyperclip.copy('\n')
+    pyperclip.copy('\n'.join(matches))
