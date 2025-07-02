0d0a96e85bccaf06ccbccb092dde56d9a9b63759
Brian kimanzi <migelbrian3@gmail.com>
2025-07-02 08:12:21 +0300

    New Automation Codes

--- Project_Phone Number and Email Extractor.py
+++ Project_Phone Number and Email Extractor.py
@@ -24,8 +24,8 @@ text = str(pyperclip.paste())
 matches = []
 for groups in phoneRegex.findall(text):
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
-    if groups[8] != '':
-        phoneNum += ' x' + groups[8]
+    if groups[7] != '':
+        phoneNum += ' x' + groups[7]
     matches.append(phoneNum)
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
