0177f7143fb213b45fb338daab1c60265086289c
Brian kimanzi <migelbrian3@gmail.com>
2025-07-02 09:13:34 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/The current working Directory.py
+++ Chapter8 Reading and writing Files/The current working Directory.py
@@ -2,5 +2,5 @@ import os
 
 print(os.getcwd())  # returns the current working directory as string
 
-os.chdir('/user/brian')  # changes the current directory
+os.chdir('/Users/briankimanzi/Documents/')  # changes the current directory
 print(os.getcwd())
