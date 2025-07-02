f9beb0b17205f70244ee0836fd7c6d7999b47b94
Brian kimanzi <migelbrian3@gmail.com>
2025-07-02 09:25:54 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Absolute vs Relative paths.py
+++ Chapter8 Reading and writing Files/Absolute vs Relative paths.py
@@ -1,3 +1,3 @@
-# Absolute path = which always begins with the root folder
-# A relative path = which is relative to the program’s current working directory
+# Absolute path = which always begins with the root folder eg (\bacon\fizz)
+# A relative path = which is relative to the program’s current working directory eg (.\fizz\spam.txt or ..\)
 
