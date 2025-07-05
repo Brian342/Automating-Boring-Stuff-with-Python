4eb7b4603f3be7833c55745b0abb779f67af9917
Brian kimanzi <migelbrian3@gmail.com>
2025-07-05 17:58:56 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Reading the contents of Files.py
+++ Chapter8 Reading and writing Files/Reading the contents of Files.py
@@ -1,1 +1,4 @@
-path = '/Users/briankimanzi/Documents/programming Languages/PythonProgramming/Automating-Boring-Stuff-with-Python/Chapter8 Reading and writing Files/Hello', 'r'
+HelloFile = open('/Users/briankimanzi/Documents/programming Languages/PythonProgramming/Automating-Boring-Stuff-with-Python/Chapter8 Reading and writing Files/Hello', 'r')
+
+HelloContent = HelloFile.read()
+print(HelloContent)
