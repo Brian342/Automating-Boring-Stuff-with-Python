e8d3143804ec1b927b1e82670a74dbf9901d9d19
Brian kimanzi <migelbrian3@gmail.com>
2025-07-05 17:05:17 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Finding file sizes and folder contents.py
+++ Chapter8 Reading and writing Files/Finding file sizes and folder contents.py
@@ -9,4 +9,4 @@ print(os.listdir(path))  # returns a list of filename strings for each file in t
 # Getting all the total size of all the files in this directory
 totalsize = 0
 for filename in os.listdir('/Users/briankimanzi'):
-    totalsize = totalsize + os.path.getsize(os.join)
+    totalsize = totalsize + os.path.getsize(os.path.join())
