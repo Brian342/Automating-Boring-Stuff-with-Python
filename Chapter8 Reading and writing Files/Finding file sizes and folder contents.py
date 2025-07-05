1b1e6f8b1837e9475f69b06a3549a64cc60d0fe0
Brian kimanzi <migelbrian3@gmail.com>
2025-07-05 17:02:41 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Finding file sizes and folder contents.py
+++ Chapter8 Reading and writing Files/Finding file sizes and folder contents.py
@@ -5,3 +5,5 @@ path = '/Users/briankimanzi/Documents/programming Languages/PythonProgramming/Au
 # print(os.path.getsize(path))  # returns the file in bytes of the file in the path argument
 
 print(os.listdir(path))  # returns a list of filename strings for each file in the path argument
+
+# getting all the total size of all the files in this directory
