27b9d96ee71cd7141a9c30f5d6e51cb53994151c
Brian kimanzi <migelbrian3@gmail.com>
2025-07-05 17:15:47 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Checking path Validity.py
+++ Chapter8 Reading and writing Files/Checking path Validity.py
@@ -1,4 +1,5 @@
 import os
 
 print(os.path.exists('/Users/briankimanzi/Documents/programming Languages/PythonProgramming/Automating-Boring-Stuff-with-Python/Chapter8 Reading and writing Files'))
+# the above code will return True if the file or folder referred to in the argument exists and False if it doesn't
 
