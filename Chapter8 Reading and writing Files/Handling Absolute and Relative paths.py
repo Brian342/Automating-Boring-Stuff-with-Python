7640e7b0a6029c786dce707ea74e34e1173353bb
Brian kimanzi <migelbrian3@gmail.com>
2025-07-05 16:24:12 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Handling Absolute and Relative paths.py
+++ Chapter8 Reading and writing Files/Handling Absolute and Relative paths.py
@@ -3,4 +3,6 @@ import os.path
 print(os.path.abspath('.'))  # returns a string of the absolute path of the argument an easy way to convert
 # a relative path into an absolute path
 
+print(os.path.isabs())
+
 
