48a81c08794b72335e30bb3292ab565017846d3e
Brian kimanzi <migelbrian3@gmail.com>
2025-07-05 17:45:13 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/The filereading_writing process.py
+++ Chapter8 Reading and writing Files/The filereading_writing process.py
@@ -1,5 +1,5 @@
 import os
 ### Opening Files with open() Function ###
-HelloFile = open('/Users/briankimanzi/Documents/programming Languages/PythonProgramming/Automating-Boring-Stuff-with-Python/Chapter8 Reading and writing Files/Hello')
+HelloFile = open(r'/Users/briankimanzi/Documents/programming Languages/PythonProgramming/Automating-Boring-Stuff-with-Python/Chapter8 Reading and writing Files/Hello')
 
 print(HelloFile)
