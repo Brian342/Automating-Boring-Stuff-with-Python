31c0fe5916e8a1ccffb84d0bace8a5211adc38aa
Brian kimanzi <migelbrian3@gmail.com>
2025-07-09 14:26:47 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Reading the contents of Files.py
+++ Chapter8 Reading and writing Files/Reading the contents of Files.py
@@ -3,5 +3,5 @@
 # HelloContent = HelloFile.read()
 # print(HelloContent)
 
-SonnetFile = open('/Users/briankimanzi/Documents/programming Languages/PythonProgramming/Automating-Boring-Stuff-with-Python/Chapter8 Reading and writing Files/sonnet29.txt')
+SonnetFile = open('/Users/briankimanzi/Documents/programmingLanguages/PythonProgramming/Automating-Boring-Stuff-with-Python/Chapter8 Reading and writing Files/sonnet29.txt')
 print(SonnetFile.readlines())
