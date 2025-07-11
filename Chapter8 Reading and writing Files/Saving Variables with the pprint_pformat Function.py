99b78c1d2aa0d821d93890cb796214cccdf8b0f9
Brian kimanzi <migelbrian3@gmail.com>
2025-07-11 09:31:23 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Saving Variables with the pprint_pformat Function.py
+++ Chapter8 Reading and writing Files/Saving Variables with the pprint_pformat Function.py
@@ -3,4 +3,5 @@ import pprint
 cats = [{'name': 'zophie', 'desc': 'chubby'}, {'name': 'pooka', 'desc': 'fluffy'}]
 print(pprint.pformat(cats))
 
-
+fileObj = open('mycats.py', 'w')
+fileObj.write('cats = ' + pprint.pformat(cats) + '\n')
