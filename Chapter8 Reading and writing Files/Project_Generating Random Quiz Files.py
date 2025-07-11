48aa2ee44ca0e1bf114568763b9ea5956aa52131
Brian kimanzi <migelbrian3@gmail.com>
2025-07-11 10:09:57 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Project_Generating Random Quiz Files.py
+++ Chapter8 Reading and writing Files/Project_Generating Random Quiz Files.py
@@ -6,4 +6,7 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'phoenix,',
             'Arkansas': 'Little Rocks', 'California': 'Sacramento', 'Colorado': 'Denver',
             'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta',
             'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'indianapolis',
-            'Iowa': 'Des Moines'}
+            'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
+            'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing',
+            'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena',
+            'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton'}
