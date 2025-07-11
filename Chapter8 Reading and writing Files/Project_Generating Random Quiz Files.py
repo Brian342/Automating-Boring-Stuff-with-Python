7d940272073f2598567ed20ce4fb32dbff8ba2e8
Brian kimanzi <migelbrian3@gmail.com>
2025-07-11 10:01:23 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Project_Generating Random Quiz Files.py
+++ Chapter8 Reading and writing Files/Project_Generating Random Quiz Files.py
@@ -1,5 +1,7 @@
 # step 1: Store the quiz data in a Dictionary
 import random
+
 # The quiz data. Keys are states and values are their capitals
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'phoenix,'
-           'Arkansas':'Little Rocks'}
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'phoenix,',
+            'Arkansas': 'Little Rocks', 'California': 'Sacramento', 'Colorado': 'Denver',
+            'Connecticut': 'Hartford', 'Delaware':'Dover'}
