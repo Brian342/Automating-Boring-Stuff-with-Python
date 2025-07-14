5ba21d224ed1bef405e35bfb30e6f2dd46ed5a52
Brian kimanzi <migelbrian3@gmail.com>
2025-07-14 15:49:22 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Project_Generating Random Quiz Files.py
+++ Chapter8 Reading and writing Files/Project_Generating Random Quiz Files.py
@@ -45,6 +45,10 @@ for questionNum in range(50):
 
 # step4: write content to the quiz and answer key files
 # write the question and the answer options to the quiz file
-quizFile.write('%s. what is the capital of')
+quizFile.write('%s. what is the capital of %s?\n' % (questionNum + states[questionNum]))
+
+for i in range (4):
+    quizFile.write()
+
 
 
