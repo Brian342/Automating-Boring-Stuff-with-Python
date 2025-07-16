63448d508b523197bad6438655e4bc92cab13c9a
Brian kimanzi <migelbrian3@gmail.com>
2025-07-16 16:12:03 +0300

    New Automation Codes

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -7,35 +7,50 @@
 print("""Welcome to this Simple Calculator:
 Enter value 1 and value 2""")
 print()
-input1 = input("Enter value 1: ")
-input2 = input("Enter value 2: ")
+input1 = int(input("Enter value 1: "))
+input2 = int(input("Enter value 2: "))
 
-print("""Select 1 for addition:
+print("""
+         Select 1 for addition:
          select 2 for substitution:
          select 3 for multiplication:
          select 4 for division:""")
-select = int(input())
+print()
+select = input("Enter selection: ")
 
 
 def arithmetic(x, y):
-    return x + y
+    sum = x + y
+    print(f"sum = {sum}")
 
 
 def substitution(x, y):
-    return x - y
+    sub = x - y
+    print(f"substitution = {sub}")
 
 
 def multiplication(x, y):
-    return x * y
+    mult = x * y
+    print(f"Multiplication = {mult}")
 
 
 def division(x, y):
     try:
-        x / 0
-
+        div = x / y
+        print(f"Division = {div}")
     except ZeroDivisionError as e:
         print("Can not divide with zero")
     finally:
         print("This will always output")
 
-if
+
+if select == '1':
+    arithmetic(input1, input2)
+elif select == '2':
+    substitution(input1, input2)
+elif select == '3':
+    multiplication(input1, input2)
+elif select == '4':
+    division(input1, input2)
+else:
+    print("invalid selection")
