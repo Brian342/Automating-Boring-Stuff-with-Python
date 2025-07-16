984193eb24dee939f2a08179343013f93ddff66c
Brian kimanzi <migelbrian3@gmail.com>
2025-07-16 15:07:20 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Project Multiclipboard.py
+++ Chapter8 Reading and writing Files/Project Multiclipboard.py
@@ -1,7 +1,8 @@
 # step1: comments and shelf setup
 # mcb.pyw - saves and loads pieces of text to the clipboard
 # usage: py.exe mcb.pyw save <keyword> - Saves clipboard to keyword
-#
+# py.exe mcb.pyw <keyword> - loads keyword to clipboard
+
 import shelve, pyperclip, sys
 
 mcbShelf = shelve.open('mcb')
