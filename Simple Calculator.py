b1d98a7f0ee1d05be65a74b9e988d43aac8a40f6
Brian kimanzi <migelbrian3@gmail.com>
2025-07-16 15:56:20 +0300

    New Automation Codes

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,8 +1,38 @@
 # Description: Develop a basic calculator that can perform four primary arithmetic operations:
 # addition, subtraction, multiplication, and division.
-# Create functions for each operation.
-# Take two inputs from the user and allow them to select the desired operation.
-# Handle division by zero with appropriate error messages.
+# # Create functions for each operation.
+# # Take two inputs from the user and allow them to select the desired operation.
+# # Handle division by zero with appropriate error messages.
+#
+input1 = input("Enter value 1: ")
+input2 = input("Enter value 2: ")
 
-input1 = input("Enter value 1:")
-input2 = input("Enter value 2:")
+
+def arithmetic(x, y):
+    return x + y
+
+
+def substitution(x, y):
+    return x - y
+
+
+def multiplication(x, y):
+    return x * y
+
+
+def division(x, y):
+    try:
+        x / 0
+
+    except ZeroDivisionError as e:
+        print("Can not divide with zero")
+    finally:
+        print("This will always output")
+
+
+print("""Welcome to this Simple Calculator:
+Enter input 1 and input 2
+Select 1 for addition:
+select 2 for substitution:
+select 3 for multiplication:
+""")
