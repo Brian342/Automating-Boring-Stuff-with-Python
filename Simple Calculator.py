ec0f31f22c718a823ac1517067f33acb744048ea
Brian kimanzi <migelbrian3@gmail.com>
2025-07-16 15:47:24 +0300

    New Automation Codes

---
+++ Simple Calculator.py
@@ -0,0 +1,5 @@
+# Description: Develop a basic calculator that can perform four primary arithmetic operations:
+# addition, subtraction, multiplication, and division.
+# Create functions for each operation.
+# Take two inputs from the user and allow them to select the desired operation.
+# Handle division by zero with appropriate error messages.
