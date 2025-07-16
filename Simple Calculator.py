9311cd8e4954ba43219b6776bd5f28a9701c1ac1
Brian kimanzi <migelbrian3@gmail.com>
2025-07-16 15:48:38 +0300

    New Automation Codes

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -3,3 +3,6 @@
 # Create functions for each operation.
 # Take two inputs from the user and allow them to select the desired operation.
 # Handle division by zero with appropriate error messages.
+
+input1 = input("Enter value 1:")
+input2 = input("Enter value 2:")
