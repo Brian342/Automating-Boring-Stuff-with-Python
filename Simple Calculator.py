0575dd895ae10acf281fa48d79c7516673b61fb1
Brian kimanzi <migelbrian3@gmail.com>
2025-07-16 15:59:47 +0300

    New Automation Codes

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -4,8 +4,17 @@
 # # Take two inputs from the user and allow them to select the desired operation.
 # # Handle division by zero with appropriate error messages.
 #
+print("""Welcome to this Simple Calculator:
+Enter value 1 and value 2""")
+print()
 input1 = input("Enter value 1: ")
 input2 = input("Enter value 2: ")
+
+print("""Select 1 for addition:
+         select 2 for substitution:
+         select 3 for multiplication:
+         select 4 for division:""")
+select = int(input())
 
 
 def arithmetic(x, y):
@@ -29,10 +38,4 @@ def division(x, y):
     finally:
         print("This will always output")
 
-
-print("""Welcome to this Simple Calculator:
-Enter input 1 and input 2
-Select 1 for addition:
-select 2 for substitution:
-select 3 for multiplication:
-""")
+if
