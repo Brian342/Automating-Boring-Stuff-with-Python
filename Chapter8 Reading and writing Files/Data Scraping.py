25d0f54f55b774c4b66334b5e0518c528531c389
Brian kimanzi <migelbrian3@gmail.com>
2025-07-29 14:31:26 +0300

    New Automation Codes

---
+++ Chapter8 Reading and writing Files/Data Scraping.py
@@ -0,0 +1,9 @@
+# Task 2: Data Scraper
+# Description: Develop a web scraper to extract specific data from a website (e.g., news headlines, product prices).
+
+# Objectives:
+# Use the requests library to retrieve web page content. Parse the HTML using BeautifulSoup.
+# Extract specific data, such as article titles or product details.
+# Save the scraped data into a CSV file.
+
+import requests
