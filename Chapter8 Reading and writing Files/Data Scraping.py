7b05a83158c75873b5563f814579463cf03aa70f
Brian kimanzi <migelbrian3@gmail.com>
2025-07-31 14:11:39 +0300

    New Automation Codes

--- Chapter8 Reading and writing Files/Data Scraping.py
+++ Chapter8 Reading and writing Files/Data Scraping.py
@@ -1,11 +1,11 @@
-# Task 2: Data Scraper
-# Description: Develop a web scraper to extract specific data from a website (e.g., news headlines, product prices).
-
-# Objectives:
-# Use the request library to retrieve web page content. Parse the HTML using BeautifulSoup.
-# Extract specific data, such as article titles or product details.
-# Save the scraped data into a CSV file.
-
+# # Task 2: Data Scraper
+# # Description: Develop a web scraper to extract specific data from a website (e.g., news headlines, product prices).
+#
+# # Objectives:
+# # Use the request library to retrieve web page content. Parse the HTML using BeautifulSoup.
+# # Extract specific data, such as article titles or product details.
+# # Save the scraped data into a CSV file.
+#
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -17,20 +17,23 @@ headers = {
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
-product = soup.find_all('article', class_='prd _fb col c-prd')
+product = soup.find_all('article', class_='prd')
 
-with open('Jumia Macbook sale.csv', 'w', encoding='utf-8') as csvFile:
+with open('Jumia_Macbook_sale.csv', 'w', newline='', encoding='utf-8') as csvFile:
     writer = csv.writer(csvFile)
     writer.writerow(['Product Name', 'Price', 'Ratings'])
 
     for products in product:
-        tag_title = products.find('h3')
+        tag_title = products.find('h3', class_='name')
         tag_price = products.find('div', class_='prc')
         ratings = products.find('div', class_='stars _s')
 
-        if tag_title and tag_price and ratings:
-            print(f"{tag_title.text.strip()}, \nPrice->{tag_price.text.strip()} Ratings-> {ratings.text}", sep='')
-            writer.writerow([tag_title, tag_price, ratings])
+        if tag_title and tag_price:
+            product_name = tag_title.text.strip()
+            Price = tag_price.text.strip()
+            Ratings = ratings.text.strip() if ratings else "No rating"
 
+            print(f"{product_name} \nprice->{Price} Ratings->{Ratings}")
+            writer.writerow([product_name, Price, Ratings])
 
 
