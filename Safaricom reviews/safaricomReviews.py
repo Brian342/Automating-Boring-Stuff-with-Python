5dcb394e549e87e2039b9256d188d86d5b14b999
Brian kimanzi <migelbrian3@gmail.com>
2025-08-05 13:07:46 +0300

    New Automation Codes

---
+++ Safaricom reviews/safaricomReviews.py
@@ -0,0 +1,89 @@
+# importing modules
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from bs4 import BeautifulSoup
+import time
+import undetected_chromedriver as uc
+
+options = uc.ChromeOptions()
+options.headless = False  # Set to True if you want to hide browser
+
+driver = uc.Chrome(options=options)
+driver.get(
+    "https://www.glassdoor.com/Reviews/Safaricom-Kenya-Reviews-EI_IE304581.0,9_IL.10,15_IN130.htm?filter.iso3Language=eng")
+
+# Let the page load
+time.sleep(100)
+
+# soup = BeautifulSoup(html, 'html.parser')
+
+
+titles = []
+ratings = []
+Pros = []
+cons = []
+Feedback = []
+
+num_pages = 141
+
+for page in range(num_pages):
+    time.sleep(20)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    reviews = soup.find('div', id='ReviewsFeed')
+    review_items = reviews.select('li')
+    title = soup.title.string
+
+    for items in review_items:
+        rating_tag = items.select_one('[data-test="review-rating-label"]')
+        rating = rating_tag.get_text(strip=True) if rating_tag else 'N/A'
+
+        # extracting the title
+        title_tag = items.select_one('[data-test="review-avatar-label"]')
+        title = title_tag.get_text(strip=True) if title_tag else 'N/A'
+
+        # Description
+        Pros_tag = items.select_one('[data-test="review-text-PROS"]')
+        ProsBody = Pros_tag.get_text(strip=True) if Pros_tag else 'N/A'
+
+        Pros_title = items.find('p', class_="review-text_textTitle__h8c3S review-text_green___30m9")
+        ProsHeader = Pros_title.get_text(strip=True) if Pros_title else 'N/A'
+
+        cons_title = items.find('p', class_="review-text_textTitle__h8c3S  review-text_red__0dGPZ")
+        ConsHeader = cons_title.get_text(strip=True) if cons_title else 'N/A'
+
+        cons_tag = items.select_one('[data-test="review-text-CONS"]')
+        ConsBody = cons_tag.get_text(strip=True) if cons_tag else 'N/A'
+
+        feedback_tag = items.select_one('[data-test="review-text-FEEDBACK"]')
+        feedbackBody = feedback_tag.get_text(strip=True) if feedback_tag else 'N/A'
+
+        titles.append(title)
+        ratings.append(rating)
+        Pros.append(ProsBody)
+        cons.append(ConsBody)
+        Feedback.append(feedbackBody)
+
+        try:
+            next_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test="next-page"]'))
+            )
+            next_button.click()
+            print("Clicked the next page button")
+        except Exception as e:
+            print("Failed to find or click the next page button:", e)
+
+driver.quit()
+print(driver.title)
+print(f"Job Title: {titles}")
+print()
+print(f"Job Ratings: {ratings}")
+print()
+print(f"Pros: {Pros}")
+print()
+print(f"Cons: {cons}")
+print()
+print(f"Feedback: {Feedback}")
