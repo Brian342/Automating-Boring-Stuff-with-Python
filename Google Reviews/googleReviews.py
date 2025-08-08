8a7051767a2df02016a8966e8ee2d2b34e3f5b81
Brian kimanzi <migelbrian3@gmail.com>
2025-08-08 08:31:51 +0300

    New Automation Codes

--- Google Reviews/googleReviews.py
+++ Google Reviews/googleReviews.py
@@ -28,52 +28,52 @@ with open('googleReviews.csv', 'w', newline='', encoding='utf-8') as csvFile:
     writer = csv.writer(csvFile)
     writer.writerow(['Job Title', 'Job Ratings', 'time', 'JobStatus', 'Pros', 'Cons'])
 
-for page in range(num_pages):
-    time.sleep(10)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    reviews = soup.find('div', id='ReviewsFeed')
-    if not reviews:
-        print('No reviews found on this page .... Skipping!')
-        continue
-    review_items = reviews.select('li')
-    title = soup.title.string
+    for page in range(num_pages):
+        time.sleep(10)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        reviews = soup.find('div', id='ReviewsFeed')
+        if not reviews:
+            print('No reviews found on this page .... Skipping!')
+            continue
+        review_items = reviews.select('li')
+        title = soup.title.string
     # print(title)
 
-    for items in review_items:
-        rating_tag = items.select_one('[data-test="review-rating-label"]')
-        rating = rating_tag.get_text(strip=True) if rating_tag else 'N/A'
+        for items in review_items:
+            rating_tag = items.select_one('[data-test="review-rating-label"]')
+            rating = rating_tag.get_text(strip=True) if rating_tag else 'N/A'
 
-        # getting the timestamp
-        time_tag = items.find('span', class_="timestamp_reviewDate__dsF9n")
-        PostTime = time_tag.get_text(strip=True) if time_tag else 'N/A'
+            # getting the timestamp
+            time_tag = items.find('span', class_="timestamp_reviewDate__dsF9n")
+            PostTime = time_tag.get_text(strip=True) if time_tag else 'N/A'
 
-        # job status
-        job_tag = items.find('div',
-                             class_="text-with-icon_LabelContainer__xbtB8 text-with-icon_disableTruncationMobile__o_kha")
-        job = job_tag.get_text(strip=True) if job_tag else 'N/A'
+            # job status
+            job_tag = items.find('div',
+                                class_="text-with-icon_LabelContainer__xbtB8 text-with-icon_disableTruncationMobile__o_kha")
+            job = job_tag.get_text(strip=True) if job_tag else 'N/A'
 
-        # location
-        # location_tag = items.find_next('div',
-        #                                class_="text-with-icon_TextWithIcon__5ZZqT")
-        # location = location_tag.get_text(strip=True) if location_tag else 'N/A'
+            # location
+            # location_tag = items.find_next('div',
+            #                                class_="text-with-icon_TextWithIcon__5ZZqT")
+            # location = location_tag.get_text(strip=True) if location_tag else 'N/A'
 
-        # extracting the title
-        title_tag = items.select_one('[data-test="review-avatar-label"]')
-        title = title_tag.get_text(strip=True) if title_tag else 'N/A'
+            # extracting the title
+            title_tag = items.select_one('[data-test="review-avatar-label"]')
+            title = title_tag.get_text(strip=True) if title_tag else 'N/A'
 
-        # Description
-        Pros_tag = items.select_one('[data-test="review-text-PROS"]')
-        ProsBody = Pros_tag.get_text(strip=True) if Pros_tag else 'N/A'
+            # Description
+            Pros_tag = items.select_one('[data-test="review-text-PROS"]')
+            ProsBody = Pros_tag.get_text(strip=True) if Pros_tag else 'N/A'
 
-        Pros_title = items.find('p', class_="review-text_textTitle__h8c3S review-text_green___30m9")
-        ProsHeader = Pros_title.get_text(strip=True) if Pros_title else 'N/A'
+            Pros_title = items.find('p', class_="review-text_textTitle__h8c3S review-text_green___30m9")
+            ProsHeader = Pros_title.get_text(strip=True) if Pros_title else 'N/A'
 
-        cons_title = items.find('p', class_="review-text_textTitle__h8c3S  review-text_red__0dGPZ")
-        ConsHeader = cons_title.get_text(strip=True) if cons_title else 'N/A'
+            cons_title = items.find('p', class_="review-text_textTitle__h8c3S  review-text_red__0dGPZ")
+            ConsHeader = cons_title.get_text(strip=True) if cons_title else 'N/A'
 
-        cons_tag = items.select_one('[data-test="review-text-CONS"]')
-        ConsBody = cons_tag.get_text(strip=True) if cons_tag else 'N/A'
+            cons_tag = items.select_one('[data-test="review-text-CONS"]')
+            ConsBody = cons_tag.get_text(strip=True) if cons_tag else 'N/A'
 
         # try:
         #     dropDown_button = items.find_element(By.CSS_SELECTOR, '.expand-button_ExpandButton__Wevvg')
@@ -89,15 +89,17 @@ for page in range(num_pages):
         # feedback_tag = items.select_one('[data-test="review-text-FEEDBACK"]')
         # feedbackBody = feedback_tag.get_text(strip=True) if feedback_tag else 'N/A'
 
-        titles.append(title)
-        ratings.append(rating)
-        timeStamp.append(PostTime)
-        JobStatus.append(job)
-        # locationStatus.append(location)
-        Pros.append(ProsBody)
-        cons.append(ConsBody)
+            titles.append(title)
+            ratings.append(rating)
+            timeStamp.append(PostTime)
+            JobStatus.append(job)
+            # locationStatus.append(location)
+            Pros.append(ProsBody)
+            cons.append(ConsBody)
 
-        # Feedback.append(feedbackBody)
+            # Feedback.append(feedbackBody)
+            writer.writerow([titles, ratings, timeStamp,JobStatus, Pros, cons])
+            print(f"wrote review: {titles} - {ratings} - {timeStamp} - {JobStatus} - {Pros} - {cons}")
 
     try:
         next_button = WebDriverWait(driver, 10).until(
@@ -115,12 +117,12 @@ for page in range(num_pages):
 
 driver.quit()
 
-with open('googleReviews.csv', 'w', newline='', encoding='utf-8') as csvFile:
-    writer = csv.writer(csvFile)
-    writer.writerow(['Job Title', 'Job Ratings', 'time', 'JobStatus', 'Pros', 'Cons'])
-    for i in range(len(titles)):
-        writer.writerow([titles[i], ratings[i], timeStamp[i],
-                         JobStatus[i], Pros[i], cons[i]])
+# with open('googleReviews.csv', 'w', newline='', encoding='utf-8') as csvFile:
+#     writer = csv.writer(csvFile)
+#     writer.writerow(['Job Title', 'Job Ratings', 'time', 'JobStatus', 'Pros', 'Cons'])
+#     for i in range(len(titles)):
+#         writer.writerow([titles[i], ratings[i], timeStamp[i],
+#                          JobStatus[i], Pros[i], cons[i]])
 
 print(f"\nTotal Reviews Scraped: {len(titles)}")
 print(f"Job Title: {titles}")
